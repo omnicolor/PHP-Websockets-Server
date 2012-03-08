133c7d0bd3d380fdc1b2c5b8653ddee002f58fc1
Omni Adams <omni@digitaldarkness.com>
2012-03-07 20:00:47 -0600

    Moved Foo client to WsEcho client.

---
+++ WsEcho/WsEcho.class.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Sample Application that implements a simple echo but shows
+ * how to add services through a simple class definition.
+ */
+require_once "./lib/wsapp.interface.php";
+
+
+class WsEcho implements WSApp {
+    public static $app_name = 'wsecho';
+
+    private $protocol;
+
+    function onMessage($msg) {
+        $size = $msg['size'];
+        $data = $msg['frame'];
+        echo "In WsEcho Class onMessage: just received ".$data." \n";
+        // Just echo it back
+        $this->protocol->send($msg);
+    }
+
+    function onClose() {
+        echo "In WsEcho Class onClose: closing connection \n";
+    }
+
+    function onError($err) {
+        echo "In WsEcho Class onError: closing connection \n";
+    }
+
+    function setProtocol(WSProtocol $protocol) {
+        $this->protocol = $protocol;
+    }
+}
