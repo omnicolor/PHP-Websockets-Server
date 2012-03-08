95c789de19018c09c529ee6af0a19502c6d19a45
Omni Adams <omni@digitaldarkness.com>
2012-03-07 18:27:31 -0600

    De-daemonized the file and code cleanup.

--- WSdaemon.php
+++ WSdaemon.php
@@ -1,16 +1,13 @@
-#!/usr/bin/php
 <?php
-
-/***
-* Make adjustments for your install
-*
-*/
+/**
+ * Web socket daemon file.
+ */
 
 require_once "lib/wsutil.php";
 require_once "lib/websocket.class.php";
 require_once "lib/wsuser.class.php";
 
-$log = '/var/log/websockets.log';
+$log = '/tmp/websockets.log';
 $port = '8088';
 $address = "127.0.0.1";
 
@@ -54,43 +51,31 @@ if($argc > 0) {
 	}
 }
 
-//fork the process to work in a daemonized environment
-if (! function_exists('pcntl_fork')) die('PCNTL functions not available on this PHP installation');
-logToFile("Status: WS starting up.\n");
-$pid = pcntl_fork();
-if($pid == -1) {
-	logToFile("Error could not daemonize process.\n");
-	return 1; //error
-} else if($pid) {
-	return 0;
-} else {
-	error_reporting(E_ALL);
-	set_time_limit(0);
-	ob_implicit_flush();
+error_reporting(E_ALL);
+set_time_limit(0);
+ob_implicit_flush();
 
-	$master = GetSocket($address, $port, $log);
-	$sockets = array($master);
-	$users = array();
+$master = GetSocket($address, $port, $log);
+$sockets = array($master);
+$users = array();
 
-	// The main process
-	while(true) {
-		$changed = $sockets;
-		socket_select($changed, $write = NULL, $except = NULL, NULL);
-		foreach($changed as $socket) {
-			if($socket == $master) {
-				$client = socket_accept($master);
-				if($client < 0) { logToFile("socket_accept() failed");
-					continue ;
-				} else {
-					WsConnect($client, $sockets, $users);
-					logToFile($client." CONNECTED\n");
-				}
+// The main process
+while(true) {
+	$changed = $sockets;
+	socket_select($changed, $write = NULL, $except = NULL, NULL);
+	foreach($changed as $socket) {
+		if($socket == $master) {
+			$client = socket_accept($master);
+			if($client < 0) { logToFile("socket_accept() failed");
+				continue ;
 			} else {
-				$user = WsGetUserBySocket($socket, $users);
-				$ws = new WebSocket($user, $log);
-				$ws->handleRequest($socket);
+				WsConnect($client, $sockets, $users);
+				logToFile($client." CONNECTED\n");
 			}
+		} else {
+			$user = WsGetUserBySocket($socket, $users);
+			$ws = new WebSocket($user, $log);
+			$ws->handleRequest($socket);
 		}
 	}
 }
-?>
