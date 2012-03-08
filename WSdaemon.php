45433e60e7f1485622a23e77e654865b558e4b47
Omni Adams <omni@digitaldarkness.com>
2012-03-08 07:25:12 -0600

    Changed tabs to spaces.

--- WSdaemon.php
+++ WSdaemon.php
@@ -15,40 +15,40 @@ $address = "127.0.0.1";
  * Method for displaying the help and default variables
  */
 function displayUsage() {
-	global $log, $port, $address;
+    global $log, $port, $address;
 
-	echo "\n";
-	echo "PHP Web Sockets Server\n";
-	echo "\n";
-	echo "Usage:\n";
-	echo "\tWSdaemon.php [options\n]";
-	echo "\n";
-	echo "\toptions:\n";
-	echo "\t\t--help display this help message\n";
-	echo "\t\t--log=<filename> The location of the log file (default '$log')\n";
-	echo "\t\t--port=<port> The port on which the server should listen (default '$port')\n";
-	echo "\t\t--address=<IP Address> The local address on which to listen (default '$address')\n";
-	echo "\n";
+    echo "\n";
+    echo "PHP Web Sockets Server\n";
+    echo "\n";
+    echo "Usage:\n";
+    echo "\tWSdaemon.php [options\n]";
+    echo "\n";
+    echo "\toptions:\n";
+    echo "\t\t--help display this help message\n";
+    echo "\t\t--log=<filename> The location of the log file (default '$log')\n";
+    echo "\t\t--port=<port> The port on which the server should listen (default '$port')\n";
+    echo "\t\t--address=<IP Address> The local address on which to listen (default '$address')\n";
+    echo "\n";
 } //end display usage
 
 //configure command line arguments
 if($argc > 0) {
-	foreach($argv as $arg) {
-		$args = explode('=', $arg);
-		switch($args[0]) {
-			case '--help' :
-				return displayUsage();
-			case '--log' :
-				$log = $args[1];
-				break;
-			case '--port' :
-				$port = $args[1];
-				break;
-			case '--address' :
-				$address = $args[1];
-				break;
-		}
-	}
+    foreach($argv as $arg) {
+        $args = explode('=', $arg);
+        switch($args[0]) {
+            case '--help' :
+                return displayUsage();
+            case '--log' :
+                $log = $args[1];
+                break;
+            case '--port' :
+                $port = $args[1];
+                break;
+            case '--address' :
+                $address = $args[1];
+                break;
+        }
+    }
 }
 
 error_reporting(E_ALL);
@@ -61,21 +61,21 @@ $users = array();
 
 // The main process
 while(true) {
-	$changed = $sockets;
-	socket_select($changed, $write = NULL, $except = NULL, NULL);
-	foreach($changed as $socket) {
-		if($socket == $master) {
-			$client = socket_accept($master);
-			if($client < 0) { logToFile("socket_accept() failed");
-				continue ;
-			} else {
-				WsConnect($client, $sockets, $users);
-				logToFile($client." CONNECTED\n");
-			}
-		} else {
-			$user = WsGetUserBySocket($socket, $users);
-			$ws = new WebSocket($user, $log);
-			$ws->handleRequest($socket);
-		}
-	}
+    $changed = $sockets;
+    socket_select($changed, $write = NULL, $except = NULL, NULL);
+    foreach($changed as $socket) {
+        if($socket == $master) {
+            $client = socket_accept($master);
+            if($client < 0) { logToFile("socket_accept() failed");
+                continue ;
+            } else {
+                WsConnect($client, $sockets, $users);
+                logToFile($client." CONNECTED\n");
+            }
+        } else {
+            $user = WsGetUserBySocket($socket, $users);
+            $ws = new WebSocket($user, $log);
+            $ws->handleRequest($socket);
+        }
+    }
 }
